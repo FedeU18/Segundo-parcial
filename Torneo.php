837b82bb26ff924e75d51d166207d5ffa7737b1e
Federico <fedee.unates.2001@gmail.com>
2024-05-24 17:15:13 -0300

    Ejercicio 7 terminado

--- Torneo.php
+++ Torneo.php
@@ -5,9 +5,9 @@ class Torneo
   private $colPartidos;
   private $premio;
 
-  public function __construct($colPartidos, $premio)
+  public function __construct($premio)
   {
-    $this->colPartidos = $colPartidos;
+    $this->colPartidos = [];
     $this->premio = $premio;
   }
 
@@ -52,6 +52,52 @@ class Torneo
       }
     }
     return $objPartido;
+  }
+
+  public function darGanadores($deporte)
+  {
+    $colGanadores = [];
+    $colPartidos = $this->getColPartidos();
+
+    if ($deporte == "futbol") {
+      foreach ($colPartidos as $partido) {
+        if ($partido instanceof PartidoFutbol) {
+          if ($partido->getCantGolesE1() > $partido->getCantGolesE2()) {
+            $colGanadores[] = $partido->getObjEquipo1();
+          } elseif ($partido->getCantGolesE1() < $partido->getCantGolesE2()) {
+            $colGanadores[] = $partido->getObjEquipo2();
+          }
+        }
+      }
+    } else {
+      foreach ($colPartidos as $partido) {
+        if ($partido instanceof PartidoBasquet) {
+          if ($partido->getCantGolesE1() > $partido->getCantGolesE2()) {
+            $colGanadores[] = $partido->getObjEquipo1();
+          } elseif ($partido->getCantGolesE1() < $partido->getCantGolesE2()) {
+            $colGanadores[] = $partido->getObjEquipo2();
+          }
+        }
+      }
+    }
+    return $colGanadores;
+  }
+
+  public function calcularPremioPartido($objPartido)
+  {
+    $coeficientePartido = $objPartido->coeficientePartido();
+    $premioPartido = $coeficientePartido * $this->getPremio();
+    $equipoGanador = null;
+    if ($objPartido->getCantGolesE1() > $objPartido->getCantGolesE2()) {
+      $equipoGanador = $objPartido->getObjEquipo1();
+    } else {
+      $equipoGanador = $objPartido->getObjEquipo2();
+    }
+    $premioDelGanador = [
+      "equipoGanador" => $equipoGanador,
+      "premioPartido" => $premioPartido
+    ];
+    return $premioDelGanador;
   }
 
   public function __toString()
